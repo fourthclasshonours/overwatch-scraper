536130fb5dd803a560f30da83eb11f02f4e43bbb
Duncan Leo <hello@duncanleo.me>
2021-03-27 18:01:51 +0800

    chore: remove sg-scraper specific code

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,22 +1,9 @@
 import 'ts-polyfill/lib/es2019-array';
 
 import puppeteer, { Browser } from 'puppeteer';
-import dbs from './sources/atm/dbs';
-import ocbc from './sources/atm/ocbc';
-import blackball from './sources/boba/blackball';
-import eachACup from './sources/boba/each-a-cup';
-import koi from './sources/boba/koi';
-import liho from './sources/boba/liho';
-import gongCha from './sources/boba/gong-cha';
-
-import { Boba } from './sources/boba/model';
-import { ATM } from './sources/atm/model';
 
 import * as Sentry from '@sentry/node';
 import { readStore, writeStore } from './output';
-import chicha from './sources/boba/chicha';
-import tigersugar from './sources/boba/tiger-sugar';
-import playmade from './sources/boba/playmade';
 
 import fs from 'fs';
 
@@ -34,36 +21,12 @@ try {
   fs.mkdirSync('traces');
 } catch (e) {}
 
-async function atm(browser: Browser) {
-  const tempFunc = async (
-    bankName: string,
-    workFunc: (browser: Browser) => Promise<ATM[]>
-  ) => {
-    try {
-      const data = readStore('banks.json');
-      writeStore('banks.json', {
-        ...data,
-        [bankName]: await workFunc(browser),
-      });
-    } catch (e) {
-      console.error(e);
-      Sentry?.captureException(e);
-    }
-  };
-
-  await Promise.all([tempFunc('DBS', dbs), tempFunc('OCBC', ocbc)]);
-}
-
-async function boba(browser: Browser) {
   async function tempFunc(
     chainName: string,
-    workFunc: (browser: Browser) => Promise<Boba[]>
   ) {
     try {
       const data = await workFunc(browser);
 
-      const store = readStore('boba.json');
-      writeStore('boba.json', {
         ...store,
         [chainName]: data,
       });
@@ -73,16 +36,6 @@ async function boba(browser: Browser) {
     }
   }
 
-  await Promise.all([
-    tempFunc('BlackBall', blackball),
-    tempFunc('Each-A-Cup', eachACup),
-    tempFunc('Gong Cha', gongCha),
-    tempFunc('Koi', koi),
-    tempFunc('LiHO', liho),
-    tempFunc('ChiCha', chicha),
-    tempFunc('Tiger Sugar', tigersugar),
-    tempFunc('Playmade', playmade),
-  ]);
 }
 
 async function scraper() {
@@ -97,8 +50,6 @@ async function scraper() {
       : undefined,
   });
 
-  await atm(browser);
-  await boba(browser);
 
   await browser.close();
 }
